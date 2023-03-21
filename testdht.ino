97b30d1e6909e2904bd65c33bbed596b7bfefc03
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2023-03-21 23:47:59 +0800

    Update testdht.ino

--- testdht.ino
+++ testdht.ino
@@ -1,28 +1,47 @@
-#include "DHT.h"
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, modified for integer use, public domain
 
-DHT dht;
+#include "TinyDHT.h"
 
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println();
-  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
+#define DHTPIN 2  // DHT connected to Arduino Uno Digital Pin 2
 
-  dht.setup(2); 	/* set pin for data communication */
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11 
+//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) 
+//    of the sensor
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600); // Output status on Uno serial monitor
+  Serial.println("DHTxx test!");
+ 
+  dht.begin();
 }
 
-void loop()
-{
-  delay(dht.getMinimumSamplingPeriod());	/* Delay of amount equal to sampling period */	
+void loop() {
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  int8_t h = dht.readHumidity();
+  int16_t t = dht.readTemperature(1);
 
-  float humidity = dht.getHumidity();	    /* Get humidity value */
-  float temperature = dht.getTemperature();	/* Get temperature value */
-
-  Serial.print(dht.getStatusString());	    /* Print status of communication */
-  Serial.print("\t");
-  Serial.print(humidity, 1);
-  Serial.print("\t\t");
-  Serial.print(temperature, 1);
-  Serial.print("\t\t");
-  Serial.println(temperature), 1);	/* Convert temperature to Fahrenheit units */
+  // check if returns are valid then something went wrong!
+  if ( t == BAD_TEMP || h == BAD_HUM ) { // if error conditions          
+    Serial.println("Failed to read from DHT");
+  } else {
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+  delay(2000);
 }
