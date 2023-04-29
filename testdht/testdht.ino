e220a1c3b47ada9af4df447e35fb438ce5cdbef5
izmeera2000 <izmeera2000@gmail.com>
2023-04-29 15:44:40 +0800

    test1

---
+++ testdht/testdht.ino
@@ -0,0 +1,77 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, modified for integer use, public domain
+
+#include "TinyDHT.h"
+
+#define DHTPIN 2 // DHT connected to Arduino Uno Digital Pin 2
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11 // DHT 11
+// #define DHTTYPE DHT22   // DHT 22  (AM2302)
+// #define DHTTYPE DHT21   // DHT 21 (AM2301)
+const int ENA_PIN = 7; // the Arduino pin connected to the EN1 pin L298N
+const int IN1_PIN = 6; // the Arduino pin connected to the IN1 pin L298N
+const int IN2_PIN = 5; // the Arduino pin connected to the IN2 pin L298N
+// Connect pin 1 (on the left) ovf the sensor to +5V
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power)
+//    of the sensor
+
+// kipas
+// rain sensor
+// suhu dht
+// motor slenoid
+// timer
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+  Serial.begin(115200); // Output status on Uno serial monitor
+  Serial.println("DHTxx test!");
+  pinMode(2, INPUT);
+  pinMode(ENA_PIN, OUTPUT);
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  digitalWrite(ENA_PIN, HIGH);
+
+  dht.begin();
+}
+
+void loop()
+{
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  int8_t h = dht.readHumidity();
+  int16_t t = dht.readTemperature(1);
+  int digital = digitalRead(2);
+
+  // check if returns are valid then something went wrong!
+  if (t == BAD_TEMP || h == BAD_HUM)
+  { // if error conditions
+    Serial.println("Failed to read from DHT");
+  }
+  else
+  {
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.println(" *C");
+    Serial.println(digital);
+
+    if (t > 29)
+    {
+      digitalWrite(IN1_PIN, HIGH);
+      digitalWrite(IN2_PIN, LOW);
+    }
+    else
+    {
+      digitalWrite(IN1_PIN, LOW);
+      digitalWrite(IN2_PIN, HIGH);
+    }
+  }
+  delay(2000);
+}
