56ec131544211ce15c399e82fd9acb839130810f
izmeera2000 <izmeera2000@gmail.com>
2023-05-13 20:22:13 +0800

    test4

--- testdht/testdht.ino
+++ testdht/testdht.ino
@@ -34,6 +34,65 @@ const int IN4_PIN = 4;  // the Arduino pin connected to the IN4 pin L298N
 
 DHT dht(DHTPIN, DHTTYPE);
 
+
+byte year;
+byte month;
+byte date;
+byte dOW;
+byte hour;
+byte minute;
+byte second;
+
+void getDateStuff(byte& year, byte& month, byte& date, byte& dOW,
+                  byte& hour, byte& minute, byte& second) {
+    // Call this if you notice something coming in on
+    // the serial port. The stuff coming in should be in
+    // the order YYMMDDwHHMMSS, with an 'x' at the end.
+    boolean gotString = false;
+    char inChar;
+    byte temp1, temp2;
+    char inString[20];
+    
+    byte j=0;
+    while (!gotString) {
+        if (Serial.available()) {
+            inChar = Serial.read();
+            inString[j] = inChar;
+            j += 1;
+            if (inChar == 'x') {
+                gotString = true;
+            }
+        }
+    }
+    Serial.println(inString);
+    // Read year first
+    temp1 = (byte)inString[0] -48;
+    temp2 = (byte)inString[1] -48;
+    year = temp1*10 + temp2;
+    // now month
+    temp1 = (byte)inString[2] -48;
+    temp2 = (byte)inString[3] -48;
+    month = temp1*10 + temp2;
+    // now date
+    temp1 = (byte)inString[4] -48;
+    temp2 = (byte)inString[5] -48;
+    date = temp1*10 + temp2;
+    // now Day of Week
+    dOW = (byte)inString[6] - 48;
+    // now hour
+    temp1 = (byte)inString[7] -48;
+    temp2 = (byte)inString[8] -48;
+    hour = temp1*10 + temp2;
+    // now minute
+    temp1 = (byte)inString[9] -48;
+    temp2 = (byte)inString[10] -48;
+    minute = temp1*10 + temp2;
+    // now second
+    temp1 = (byte)inString[11] -48;
+    temp2 = (byte)inString[12] -48;
+    second = temp1*10 + temp2;
+}
+
 void setup() {
   Serial.begin(115200);  // Output status on Uno serial monitor
   Serial.println("DHTxx test!");
@@ -52,6 +111,23 @@ void loop() {
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   int8_t h = dht.readHumidity();
   int16_t t = dht.readTemperature(1);
+
+    if (Serial.available()) {
+        getDateStuff(year, month, date, dOW, hour, minute, second);
+        
+        myRTC.setClockMode(false);  // set to 24h
+        //setClockMode(true); // set to 12h
+        
+        myRTC.setYear(year);
+        myRTC.setMonth(month);
+        myRTC.setDate(date);
+        myRTC.setDoW(dOW);
+        myRTC.setHour(hour);
+        myRTC.setMinute(minute);
+        myRTC.setSecond(second);
+    }
+
+    
 
   Serial.print(" minute :  ");
 
